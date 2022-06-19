f695fd62c4945cbc0ef8ab44a4dd1ecef159def9
sid <s.sidd707@gmail.com>
2022-06-19 13:02:10 +0545

    second commit

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.icu.lang.UCharacter;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView myRecyclerView;
     MyRecyclerAdapter myAdapter;
@@ -18,10 +20,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //<String> randomList = new ArrayList<>();
+        ArrayList<SampleModel> randomList = new ArrayList<>();
+
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("apple");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("mango");
+//        randomList.add("apple");
+
+        randomList.add(new SampleModel(
+                "neha",
+                "8755151515",
+                "Baneshwor",
+                "neha@gmail.com"
+        ));
+        randomList.add(new SampleModel(
+                "susmin",
+                "877751515",
+                "Baneshwor1",
+                "susmin@gmail.com"
+                ));
+   randomList.add(new SampleModel(
+                "jayant",
+                "8755151444",
+                "lalitpur",
+                "jayant@gmail.com"
+                ));
+   randomList.add(new SampleModel(
+                "rosy",
+                "74554415",
+                "Kritipur",
+                "rosy@gmail.com"
+                ));
+   randomList.add(new SampleModel(
+                "shristi",
+                "98555151515",
+                "kritipur",
+                "shristi@gmail.com"
+                ));
+
+
         myRecyclerView=findViewById(R.id.recycler_view);
-        myAdapter = new MyRecyclerAdapter();
+        myAdapter = new MyRecyclerAdapter(randomList);
         linearLayoutManager= new LinearLayoutManager(
-                Context:this,RecyclerView,
+               this, RecyclerView.VERTICAL, false
         );
+
+        myRecyclerView.setAdapter(myAdapter);
+        myRecyclerView.setLayoutManager(linearLayoutManager);
     }
 }
---
+++ app/src/main/java/com/example/app2/SampleModel.java
@@ -0,0 +1,47 @@
+package com.example.app2;
+
+public class SampleModel {
+    String userName;
+    String phoneNumber;
+    String address;
+    String email;
+
+    public SampleModel(String userName, String phoneNumber, String address, String email) {
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
